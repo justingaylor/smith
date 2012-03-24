2006f94a7b669f0e2ec6e97f41b7bb743a1a99d0
Justin Gaylor <justin.gaylor@gmail.com>
2012-03-25 02:15:50 +0900

    run.rb now uses a regex

--- test/run.rb
+++ test/run.rb
@@ -2,7 +2,7 @@ require File.expand_path( File.join( File.dirname(__FILE__), "helper" ) )
 
 Dir.recurse( "." ) do |file|
   filename = File.basename(file)
-  if filename[-8..-1] == "_test.rb"
+  if filename =~ /(.*)_test.rb/
     puts "-"*(file.length+6)
     puts "rspec #{file}"
     puts "-"*(file.length+6)
